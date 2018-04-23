0f2a6f319fd78ae5cb84c5daac482af0e7d05a2b
Timea Heidenreich <timea.heidenreich@gmail.com>
2018-04-23 12:21:44 -0700

    some of the functionality done

--- timea-emery/public/scripts/articleView.js
+++ timea-emery/public/scripts/articleView.js
@@ -131,9 +131,14 @@ articleView.initIndexPage = () => {
 };
 
 articleView.initAdminPage = () => {
+
+  var template = Handlebars.compile($('#author-template').text());
   
   // REVIEW: We use .forEach() here because we are relying on the side-effects of the callback function: appending to the DOM. The callback is not required to return anything.
   app.Article.numWordsByAuthor().forEach(stat => $('.author-stats').append(template(stat)));
+
+
+  console.log('hello');
 
   // REVIEW: Simply write the correct values to the page:
   $('#blog-stats .articles').text(app.Article.all.length);
